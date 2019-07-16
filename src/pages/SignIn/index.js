11c76a7f34c8c4666575255c65e013510d68a5b3
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-16 09:00:36 -0300

    Validating forms from signin and signup pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Informe um e-mail válido')
+        .required('E-mail é obrigatório'),
+    password: Yup.string().required('Senha é obrigatória'),
+});
+
 export default function SignIn() {
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
+
     return (
         <>
             <img src={logo} alt="GoBarber" />
 
-            <form>
-                <input type="email" placeholder="Seu e-mail" />
-                <input type="password" placeholder="Sua senha" />
+            <Form onSubmit={handleSubmit} schema={schema}>
+                <Input name="email" type="email" placeholder="Seu e-mail" />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Sua senha"
+                />
                 <button type="submit">Acessar</button>
                 <Link to="/register">Criar conta gratuita</Link>
-            </form>
+            </Form>
         </>
     );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+    name: Yup.string().required('O nome é obrigatório'),
+    email: Yup.string()
+        .email('Informe um e-mail válido')
+        .required('E-mail é obrigatório'),
+    password: Yup.string()
+        .min(6, 'A senha precisa ter pelo menos 6 caracteres')
+        .required('Senha é obrigatória'),
+});
+
 export default function SignUp() {
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
+
     return (
         <>
             <img src={logo} alt="GoBarber" />
 
-            <form>
-                <input placeholder="Nome completo" />
-                <input type="email" placeholder="Seu e-mail" />
-                <input type="password" placeholder="Sua senha" />
+            <Form onSubmit={handleSubmit} schema={schema}>
+                <Input name="name" placeholder="Nome completo" />
+                <Input name="email" type="email" placeholder="Seu e-mail" />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Sua senha"
+                />
                 <button type="submit">Criar conta</button>
                 <Link to="/">Já tenho login</Link>
-            </form>
+            </Form>
         </>
     );
 }
