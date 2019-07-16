c1904e7c3a96f4241e1700ce00e8445a3ccee64c
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-16 16:27:30 -0300

    Styling notifications

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Notifications from '~/components/Notifications';
+
 import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
@@ -13,6 +15,7 @@ export default function Header() {
                     <Link to="/dashboard">DASHBOARD</Link>
                 </nav>
                 <aside>
+                    <Notifications />
                     <Profile>
                         <div>
                             <strong>Diego Fernandes</strong>
