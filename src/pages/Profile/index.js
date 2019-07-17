9e9aaea6f1fba6f9e653842bae9d86e355f0ead1
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-16 23:18:16 -0300

    Updating user profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-    return <div />;
+    const profile = useSelector(state => state.user.profile);
+    const dispatch = useDispatch();
+
+    function handleSubmit(data) {
+        dispatch(updateProfileRequest(data));
+    }
+
+    return (
+        <Container>
+            <Form initialData={profile} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Nome completo" />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Seu endereço de e-mail"
+                />
+                <hr />
+                <Input
+                    type="password"
+                    name="oldPassword"
+                    placeholder="Sua senha atual"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Nova senha"
+                />
+                <Input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirmar senha"
+                />
+                <button type="submit">Atualizar perfil</button>
+            </Form>
+            <button type="button">Sair do GoBarber</button>
+        </Container>
+    );
 }
