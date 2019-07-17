c4c254b1163d2d8f5b0974376c12f7ef785588e3
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-17 12:44:10 -0300

    Listing appointments and fixing styles

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -3,7 +3,7 @@ import { darken } from 'polished';
 
 export const Container = styled.div`
     max-width: 600px;
-    margin: 50px auto 0;
+    margin: 50px auto;
 
     form {
         display: flex;
